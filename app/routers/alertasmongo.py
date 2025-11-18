39bbe51ee57029c0992446c7dd5af93b3ea36d42
Joaquin-Martinez-Aravena <109876974+Joaquin-Martinez-Aravena@users.noreply.github.com>
2025-11-17 21:03:31 -0300

    conexion bd y render

--- app/routers/alertasmongo.py
+++ app/routers/alertasmongo.py
@@ -9,7 +9,7 @@ router = APIRouter(
     tags=["Alertas Mongo"]
 )
 
-@router.post("/")
+@router.get("/")
 def crear_alerta_endpoint(body: Dict[str, Any]):
     """
     Crea una alerta desde el frontend.
